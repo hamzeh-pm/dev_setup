f5aae1a342f7456c10af2090719625e158de60cd
Hamzeh Pourmahdi <h.pourmahdi@hotmail.com>
2025-08-01 10:45:23 +0330

    fix conform plugin config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,12 +1,19 @@
-require("conform").setup({
-    formatters_by_ft = {
-        python = { "ruff_format" }, -- Use ruff as the formatter for Python
-    },
-    formatters = {
-        ruff_format = {
+return {
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          python = { "ruff_format" }, -- Use ruff as the formatter for Python
+        },
+        formatters = {
+          ruff_format = {
             command = "ruff",
             args = { "format", "--stdin-filename", "$FILENAME", "-" },
             stdin = true,
+          },
         },
-    },
-})
+      })
+    end,
+  },
+}
